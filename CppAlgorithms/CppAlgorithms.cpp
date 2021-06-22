04b93678ae02729fd99a4d9de7efeec38cea2be8
Luca Martinelli (linux) <lucxmangajet@gmail.com>
2021-06-22 10:48:54 +0200

    Added solutions from practice

--- CppAlgorithms/CppAlgorithms.cpp
+++ CppAlgorithms/CppAlgorithms.cpp
@@ -5,7 +5,7 @@
 #include <string>
 
 //forward declarations to allow main() to be the first function
-const int getMinimum(int* arr, int length);
+const int getMinimum(int arr[], int length);
 const int getMinimum(std::vector<int>* list);
 void normalizeArray(float* arr, int length);
 void normalizeArray(std::vector<float>* list);
@@ -15,8 +15,6 @@ const char* toHex(char* str, int length);
 
 template<typename T>
 void assertEqual(T value, T expected, const char* name);
-
-
 
 
 int main()
@@ -66,14 +64,50 @@ int main()
 
 
 //calculates the smallest element of an integer array
-const int getMinimum(int* arr, int length)
+const int getMinimum(int arr[], int length)
 {
-	return 0;
+	int min = INT32_MAX;
+
+	for (size_t i = 0; i < length; i++)
+	{
+		int val = *(arr + i);
+		//same as int val = arr[i];
+
+		if(val < min)
+			min = val;
+	}
+	return min;
 }
 
 const int getMinimum(std::vector<int>* list)
 {
-	return 0;
+	int min = INT32_MAX;
+
+	// version 1, treating vector like a c array	
+	for(int i = 0; i < list->size(); i++)
+	{
+		int val = *((&list->front())+i);
+
+		if(val > min)
+			min = val;
+	}
+
+	//version 2, using iterators
+	for (auto it = list->begin(); it != list->end(); ++it)
+	{
+		int val = *it;
+		if(val < min)
+			min = val;
+	}
+
+	//version 3: "for each" veriant
+	for (const auto& val : *list)
+	{
+			if(val < min)
+				min = val;
+	}
+
+	return min;
 }
 
 //takes an array, every element gets changed in the following way:
@@ -82,12 +116,28 @@ const int getMinimum(std::vector<int>* list)
 
 void normalizeArray(float* arr, int length)
 {
+	for (int i = 0; i < length; i++)
+	{
+			if(arr[i] < -1)
+				arr[i] = -1;
 
+			if(arr[i] >1)
+				arr[i] = 1;
+	}
 }
 
-void normalizeArray(std::vector<float>* list)
+void normalizeArray(std::vector<float>* listPtr)
 {
+	std::vector<float> list = *listPtr;
 
+	for (int i = 0; i < list.size(); i++)
+	{
+			if(list[i] < -1)
+				list[i] = -1;
+
+			if(list[i] >1)
+				list[i] = 1;
+	}
 }
 
 //reimplementation of standard C strlen
@@ -95,14 +145,21 @@ void normalizeArray(std::vector<float>* list)
 // C strings always end in a null terminator which has value: '\0'(char) or 0x00 (hex) or 0 (dec)
 int newstrlen(const char* str)
 {
-	return 0;
+	int i =0;
+	char current = str[0];
+
+	while(current) 
+	{
+		i++;
+		current = str[i];
+	}
+	return i;
 }
 
 int newstrlen(std::string str)
 {
-	return 0;
+	return str.length();
 }
-
 
 // converts a byte array to a C string Hex representation of it
 // for example "Hello" would become -> "48656C6C6F"
@@ -110,7 +167,16 @@ static const char  hexDigits[16] = {'0','1','2','3','4','5','6','7','8','9','A',
 
 const char* toHex(char* str, int length)
 {
-	return nullptr;
+	char* arr = new char[length*2 +1];
+
+	for (size_t i = 0; str[i]; i++)
+	{
+		arr[2*i] =  hexDigits[(str[i] & 0xF0) >> 4];
+		arr[2*i+1] = hexDigits[str[i] & 0x0F];
+	}
+		
+	arr[length*2] = 0x00; //null terminator, indicates string is finished
+	return arr;
 }
 
 
